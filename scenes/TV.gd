5ac6788fc240808b2be12882b80b494a5b935f09
Kostas Vasileiadis <k.vasileiadis@vehiculum.de>
2020-03-30 14:02:41 +0200

    Added lights and shadows to TV scene. WIP change variable names.

---
+++ scenes/TV.gd
@@ -0,0 +1,24 @@
+extends Node
+
+onready var Tween = get_node("TVMain/Tween")
+var tweenPlayback = 'straight'
+
+func _ready():
+	Tween.interpolate_property($TVMain/Light2D, "texture_scale", 1, 2, 2, Tween.TRANS_SINE, Tween.EASE_IN)
+	Tween.start()
+
+func _process(delta):
+	pass
+
+func flicker_light():
+	$TVMain/Light2D.texture_scale = smoothstep(1, 2, 0.15)
+
+func _on_Tween_tween_all_completed():
+	if tweenPlayback == 'straight':
+		Tween.interpolate_property($TVMain/Light2D, "texture_scale", 2, 1, 2, Tween.TRANS_SINE, Tween.EASE_IN)
+		Tween.start()
+		tweenPlayback = 'reverse'
+	else:
+		Tween.interpolate_property($TVMain/Light2D, "texture_scale", 1, 2, 2, Tween.TRANS_SINE, Tween.EASE_IN)	
+		Tween.start()
+		tweenPlayback = 'straight'
