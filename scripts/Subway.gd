95434511fd2e5f12b9ebf89f25eb980f4ac395a3
Kostas Vasileiadis <k.vasileiadis@vehiculum.de>
2020-05-03 21:06:52 +0200

    Replaced Tween with AnimationPlayer.

--- scripts/Subway.gd
+++ scripts/Subway.gd
@@ -8,24 +8,12 @@ var motion = 1
 const SPEED = 50
 
 func _ready():
-	tween.interpolate_property(camera, 'offset', Vector2(0, 0), Vector2(0, 3), 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	tween.start()
+	camera.get_node("AnimationPlayer").play("shake")
 	
 	
 func _process(delta):
 	motion -= 1
-	print('motion: ', motion, ' delta: ', delta)
 	$ParallaxBackground/Sky.set_motion_offset(Vector2(motion / 4, 0))
 	$ParallaxBackground/Mountains.set_motion_offset(Vector2(motion / 2, 0))
 	$ParallaxBackground/Skyline_front.set_motion_offset(Vector2(motion, 0))
 
-
-func _on_CameraTween_tween_completed(object, key):
-	if reverse:
-		tween.interpolate_property(camera, 'offset', Vector2(0, 0), Vector2(0, 3), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-		tween.start()
-		reverse = false
-	else:
-		tween.interpolate_property(camera, 'offset', Vector2(0, 3), Vector2(0, 0), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-		tween.start()
-		reverse = true
